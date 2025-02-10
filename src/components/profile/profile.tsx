7700357116405db7d4629d0645992b45b5abe009
saadcraft <saad.crazy13@gmail.com>
2025-02-10 16:29:06 +0100

    chouiya profil

--- src/components/profile/profile.tsx
+++ src/components/profile/profile.tsx
@@ -9,15 +9,15 @@ export default function Profile({ user }: { user: Users }) {
                 <Link href="/dashboard" className='font-semibold text-third'>Tableau de bord /</Link>
                 <h1 className='font-bold text-xl'>Profile</h1>
             </div>
-            <div className='relative top-10 p-10 pb-20 max-w-5xl mx-auto bg-white rounded-md shadow-md'>
+            <div className='relative top-20 md:top-10 p-10 pb-20 max-w-5xl mx-auto bg-white rounded-md shadow-md'>
                 <div className='absolute left-0 -top-20 flex justify-center w-full'>
                     <div className='rounded-full p-4 bg-slate-100 shadow-md'>
                         <FaRegUser className='text-8xl' />
                     </div>
                 </div>
                 <p className='font-semibold text-3xl p-3 text-center'>{user.last_name + " " + user.first_name}</p>
-                <div className="flex justify-center items-center h-5/6 gap-10 rounded-lg">
-                    <div className="font-semibold text-lg space-y-4 text-gray-700">
+                <div className="flex justify-center items-center h-5/6 gap-10 rounded-lg text-xs md:text-lg">
+                    <div className="font-semibold space-y-4 text-gray-700">
                         <p>email :</p>
                         <p>Télephone :</p>
                         <p>Role:</p>
@@ -25,7 +25,7 @@ export default function Profile({ user }: { user: Users }) {
                         <p>Wilaya :</p>
                         <p>Lieux :</p>
                     </div>
-                    <div className="text-lg border-l pl-4 space-y-4 text-gray-600">
+                    <div className="border-l pl-4 space-y-4 text-gray-600">
                         <p>{user.email}</p>
                         <p>{user.phone_number_1}</p>
                         <p>{user.role}</p>
